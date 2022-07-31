c19d81944be3d7947d55e78733d3906346b4182a
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2022-07-31 21:53:38 +0530

    hemon game started

--- src/pages/hemongame/HemonGame.jsx
+++ src/pages/hemongame/HemonGame.jsx
@@ -1,7 +1,50 @@
 import './hemongame.css';
+import Navbar from '../../components/Navbar/Navbar';
 
 export default function HemonGame() {
     return (
-        <div>HemonGame</div>
+        <div className="mainbox">
+            <Navbar></Navbar>
+            <div className='gameBox'>
+                <div className='matrix'>
+                    <div className='row'>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                        <div className='column'></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
